875a83defad689a8ae2254adacab80e5bb7e47a8
br-monteiro <edson.monteiro@chaordicsystems.com>
2019-01-11 21:43:28 -0200

    Refactoring load method

--- src/Files.php
+++ src/Files.php
@@ -7,6 +7,10 @@ use App\Exceptions\UnreadableFileException;
 class Files
 {
 
+    const AS_STRING = 'string';
+    const AS_ARRAY = 'array';
+    const AS_OBJECT = 'object';
+
     /**
      * @var string The base path
      */
@@ -20,19 +24,25 @@ class Files
     /**
      * Load the file and returns the content
      * @param string $path The path to file (Based on root project) 
-     * @param bool $asArray Return the content as Array
-     * @return string|array The content File
+     * @param string $returnType The type of return content
+     * @return string|array|\stdClass The content file
      * @throws UnreadableFileException
      */
-    public function load(string $path, bool $asArray = false)
+    public function load(string $path, string $returnType = 'string')
     {
         $fullPath = $this->validateFile($path);
 
-        if ($asArray) {
+        if ($returnType === self::AS_ARRAY) {
             return file($fullPath);
         }
-        
-        return file_get_contents($fullPath);
+
+        $content = file_get_contents($fullPath);
+
+        if ($returnType === self::AS_OBJECT) {
+            return json_decode($content);
+        }
+
+        return $content;
     }
 
     /**
@@ -43,7 +53,7 @@ class Files
      */
     private function validateFile(string $path): string
     {
-        $fullPath = $this->basePath . $path;
+        $fullPath = $this->basePath . cfg::DS . $path;
 
         if (file_exists($fullPath) && !is_readable($fullPath)) {
             throw new UnreadableFileException("O arquivo {$fullPath} não pode ser lido");
