97b22bbc204c39719d5faa890a4c32739dec5bd4
br-monteiro <edson.monteiro@chaordicsystems.com>
2019-01-11 23:22:26 -0200

    Add template tags to replace

--- src/Configuration.php
+++ src/Configuration.php
@@ -6,4 +6,10 @@ class Configuration
     const DS = DIRECTORY_SEPARATOR;
     const REFERENCE = __DIR__;
     const DISCARDED = 'src';
+    const CARD_NAME = '%CARD_NAME%';
+    const CARD_URL = '%CARD_URL%';
+    const CARD_MEMBERS = '%CARD_MEMBERS%';
+    const CARD_DESCRIPTION = '%CARD_DESCRIPTION%';
+    const CARD_SOLUTION = '%CARD_SOLUTION%';
+    const CARD_OBSERVATIONS = '%CARD_OBSERVATIONS%';
 }
